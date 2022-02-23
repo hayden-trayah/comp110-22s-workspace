634b9035503c040952d99fa318b1ea793d2612ed
Hayden Trayah <haydent@live.unc.edu>
2022-02-22 21:13:14 -0500

    noice

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,3 +1,42 @@
 """Utils file used for exercise 05."""
 
 __author__ = "730325581"
+
+
+def only_evens(numbers: list[int]) -> list[int]:
+    """Outputs the even integers in the list."""
+    output_list: list[int] = []
+    for x in numbers:
+        if x % 2 == 0:
+            output_list.append(x)
+    return output_list
+        
+
+def sub(nums: list[int], start_index: int, end_index: int) -> list[int]:
+    """Returns the items in a list between the specified indices."""
+    if len(nums) == 0 or start_index >= len(nums) or end_index <= 0:
+        return []
+    
+    if start_index < 0:
+        start_index = 0
+    
+    if end_index > len(nums):
+        end_index = len(nums)
+    output_list: list[int] = []
+    i: int = 0
+    for x in nums:
+        if i >= start_index and i <= end_index - 1:
+            output_list.append(nums[i])
+        i += 1
+    return output_list
+
+
+def concat(int_list: list[int], int_list2: list[int]) -> list[int]:
+    """Combines the two inputed lists."""
+    output_list: list[int] = []
+    for x in int_list:
+        output_list.append(x)
+    
+    for x in int_list2:
+        output_list.append(x)
+    return output_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,53 @@
 """Utils test file for exercise 05."""
 
 __author__ = "730325581"
+
+from utils import only_evens, sub, concat
+
+
+def test_only_evens_edgecase() -> None:
+    """Tests the scenerio of an empty list edge case."""
+    test_list: list[int] = []
+    assert only_evens(test_list) == []
+
+
+def test_only_evens_usecase1() -> None:
+    """Tests both even and odd numbers in a list."""
+    test_list: list[int] = [1, 2, 3, 4, 5]
+    assert only_evens(test_list) == [2, 4]
+
+
+def test_only_evens_usecase2() -> None:
+    """Tests only odd numbers."""
+    test_list: list[int] = [1, 3, 5]
+    assert only_evens(test_list) == []
+
+
+def test_sub_edgecase() -> None:
+    """Tests the scenerio where the start index is greater than the length of the list."""
+    assert sub([1, 2, 3], 5, 2) == []
+
+
+def test_sub_usecase1() -> None:
+    """Tests a typical use scenerio of the sub function."""
+    assert sub([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
+
+
+def test_sub_usecase2() -> None:
+    """Tests another typical use scenerio of the sub function."""
+    assert sub([1, 2, 3, 4, 5, 6, 7, 8], 1, 4) == [2, 3, 4]  
+
+
+def test_concat_edgecase() -> None:
+    """Tests the scenerio where one list is empty."""
+    assert concat([1, 2, 3], []) == [1, 2, 3]
+
+
+def test_concat_usecase1() -> None:
+    """Tests a typical scenerio of the concat function."""
+    assert concat([233, 347, 110], [400, 410, 420, 470]) == [233, 347, 110, 400, 410, 420, 470]
+
+
+def test_concat_usecase2() -> None:
+    """Tests another typical scenerio of the concat function."""
+    assert concat([1], [99, 98, 97, 96]) == [1, 99, 98, 97, 96]
